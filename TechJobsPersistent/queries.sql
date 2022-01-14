ba50529a1d97a45aa671f9fb23e71600c81d0185
unknown <alowery299@gmail.com>
2022-01-13 22:17:26 -0600

    added queries

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,12 +1,26 @@
 --Part 1
 /*list the columns and their data types in the Jobs table.
 SELECT * FROM tech_jobs.jobs;
+Id INT
+Name VARCHAR
+EmployeeId 
+
 
 
 --Part 2
 /*write a query to list the names of the employers in St. Louis City
+SELECT Name
+FROM employers
+WHERE Location="St.Louis City"
+
+
 
 --Part 3
 /*write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order. 
 If a skill does not have a job listed, it should not be included in the results of this query.
 
+SELECT skills, Name, Description
+FROM skills
+INNER JOIN jobs
+ON skills.Id =  jobs.Id
+ORDER BY Name ASC;
